27a5e3221558b441409d5e7b64091ae867b5c2d8
Matt Brennan <mattyb1000@gmail.com>
2015-08-06 23:05:55 +0100

    mkdirp, config

--- transform.js
+++ transform.js
@@ -2,8 +2,18 @@ var σ = require('highland');
 var ttf2eot = require('ttf2eot');
 var fs = require('fs');
 var throughWritable = require('@quarterto/through-writable');
+var path = require('path');
+var defaults = require('defaults');
+var mkdirp = σ.wrapCallback(require('mkdirp'));
+
+var defaultOptions = {
+	fontDir: 'fonts'
+};
 
 module.exports = function(file, opts) {
+	var base = path.basename(file, '.ttf');
+	var options = defaults(opts, defaultOptions);
+
 	return σ.pipeline(
 		σ.map(function(ttf) {
 			return new Uint8Array(ttf);
@@ -11,6 +21,11 @@ module.exports = function(file, opts) {
 		σ.map(function(buf) {
 			return new Buffer(ttf2eot(buf).buffer);
 		}),
-		throughWritable(fs.createWriteStream('foo.eot'))
+		σ.flatMap(function(s) {
+			return mkdirp(options.fontDir, {}).map(s);
+		}),
+		throughWritable(function() {
+			return fs.createWriteStream(path.join(options.fontDir, base + '.eot'))
+		})
 	);
 };
